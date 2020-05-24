942207458e2611f18091a9778dbfe494048eb57d
tawanda-maseva <56193179+tawanda-maseva@users.noreply.github.com>
2020-05-24 22:55:15 +0200

    DEBUG = TRUE on Heroku

--- ttweets_project/settings.py
+++ ttweets_project/settings.py
@@ -149,11 +149,10 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     # Allow only Heroku to host the project.
     ALLOWED_HOSTS = ['top-tweets.herokuapp.com']
 
-    DEBUG = False
+    DEBUG = True
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    #STATIC_ROOT = 'staticfiles'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
